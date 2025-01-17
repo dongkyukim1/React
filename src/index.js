85491d87a70f873fb6c661ab0ef40e583bb57bae
dongkyukim1 <wlsntus55@gmail.com>
2025-01-17 11:20:17 +0900

    3nd Commit 리액트_연습_1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Test from './practice/components/test.jsx';
+import Test1 from "./practice/components/test1";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Test />
+        <Test1 />
     </React.StrictMode>
+
 );
